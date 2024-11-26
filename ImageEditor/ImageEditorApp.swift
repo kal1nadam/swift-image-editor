bfde3ef0f937f5efe22a27eb0d763bc04e7a9932
kal1nadam <kalinaadam75@seznam.cz>
2024-11-26 13:10:54 +0100

    Initial Commit

---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Adam Kalina on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
